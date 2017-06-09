539de6055fcdf27c903a57a8a18b6f2ba5077627
Fan <fan.yu@philips.com>
2017-06-09 13:51:13 +0800

    update

--- source/mongo/mongoTest.py
+++ source/mongo/mongoTest.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
+import datetime
 
 class MongoStore(object):
     def __init__(self,connectionStr):
         self.client=MongoClient(connectionStr)
-        self.db=self.client['blog']
-        self.collection=self.db['articles']
+        self.db=self.client['PyMongoDB']
+        self.collection=self.db['test']
     
     def insert(self,obj):
         return self.collection.insert_one(obj).inserted_id
@@ -12,3 +13,14 @@ class MongoStore(object):
     def insert_all(self,objs):
         result=self.collection.insert_many(objs)
         return result.inserted_ids
+
+store=MongoStore('mongodb://super:super@localhost:27017/')
+
+testData={
+    "name":"yfann",
+    "desc":"芝士就是力量",
+    "favor":["mongodb","python","c#"],
+    "createTime":datetime.datetime.utcnow()
+}
+
+store.insert(testData)
